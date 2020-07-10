616d00e9b6d8c2e4a7dce65e9f78923f6e7b606a
Takuya Kikuchi <kikuchi.takuya.dbce8@gmail.com>
2020-07-10 15:13:02 +0900

    Auth request

--- app/controllers/graphql_controller.rb
+++ app/controllers/graphql_controller.rb
@@ -21,6 +21,19 @@ class GraphqlController < ApplicationController
 
   private
 
+  # gets current user from token stored in the session
+  def current_user
+    # if we want to change the sign-in strategy, this is the place to do it
+    return unless session[:token]
+
+    crypt = ActiveSupport::MessageEncryptor.new(Rails.application.credentials.secret_key_base.byteslice(0..31))
+    token = crypt.decrypt_and_verify session[:token]
+    user_id = token.gsub('user-id:', '').to_i
+    User.find user_id
+  rescue ActiveSupport::MessageVerifier::InvalidSignature
+    nil
+  end
+
   # Handle form data, JSON body, or a blank value
   def ensure_hash(ambiguous_param)
     case ambiguous_param
